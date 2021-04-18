14c08961932d219168cafe1c81efcab7a7a54ad1
gomathishankar28 <gomathishankar28@gmail.com>
2021-04-18 20:37:42 +0000

    added google maps to loaction and made changes to map.js and getAddress.js

--- assets/js/getAddress.js
+++ assets/js/getAddress.js
@@ -1,17 +1,17 @@
 document.getElementById('location').addEventListener('change', function() {
     var location = this.value;
     if(location === "Eindhoven") {
-        document.getElementById("address").value = "132 zanddreef Eindhoven";
+        document.getElementById("address").value = "Wilhelminaplein, 5611 JC Eindhoven";
     } else if(location === "Uterecht") {
         console.log("you chose uterect");
-        document.getElementById("address").value = "133 landdreef uterecht";
+        document.getElementById("address").value = "Vredenburg, 3511 CW Utrecht";
     } else if(location === "Amsterdam") {
         
-        document.getElementById("address").value = "134 zanddreef Amsterdam";
+        document.getElementById("address").value = "Jan van Galenstraat 4, 1051 KM Amsterdam";
     } else if(location === "DenBosch") {
-        document.getElementById("address").value = "135 zanddreef DenBosch";
+        document.getElementById("address").value = "Markt, 5211 GA 's-Hertogenbosch";
     } else {
-        document.getElementById("address").value = "136 zanddreef mastricht";
+        document.getElementById("address").value = "Mosae Forum 99, 6211 DW Maastricht";
     }
   
 });
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 // Function display and hide store info if click on marker
-function displayMarkerInfo() {
+function displayMarkerInfo(){
     /** add class to hide all elements with store class - orginal code with modification from: https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript */
     var elems = document.querySelectorAll(".location");
     [].forEach.call(elems, function(el) {
@@ -9,44 +9,49 @@ function displayMarkerInfo() {
     document.getElementById(this.city).classList.remove('hide');
 }
 
-
+/** Declare a function to create a map - initMap function original code https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript with modifications for project */
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
-        center: { lat: 52.370216, lng: 4.895168 }
+        center: { lat: 52.373170, lng: 4.890660}, /*to find lat and lng of an address-https://www.latlong.net/convert-address-to-lat-long.html*/
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      var locations = [
-        {'location': { lat: 52.355480, lng: 4.893130 }, 
+    //Add marker locations and storeIds
+    var markerList = [
+        {'location': { lat: 51.9082773, lng: 4.60036 }, 
          'city': 'Amsterdam'
         },
-        {'location': { lat: 52.355380, lng: 4.893140 }, 
+        {'location': { lat: 51.7640606, lng: 4.7718492 }, 
         'city': 'Uterecht'
         },
-        {'location': { lat: 52.355280, lng: 4.893150 }, 
+        {'location': { lat: 51.5631166, lng: 5.2608243 }, 
         'city': 'DenBosch'
         },
-        {'location': { lat: 52.355180, lng: 4.893160 }, 
+        {'location': { lat: 51.4430834, lng: 5.4019321 }, 
         'city': 'Eindhoven'
         },
-        {'location': { lat: 52.355580, lng: 4.893120 }, 
+        {'location': { lat: 51.1487336, lng: 5.4255067 }, 
         'city': 'Mastricht'
         }
     ];
 
 
-     var markers = locations.map(function(location, i) {
-        newMarker = new google.maps.Marker({
-            position: locations.location,
-            city: locations.city,
-            label: labels[i % labels.length]
+     var markers = markerList.map((marker, i) => {
+        newMarker= new google.maps.Marker({
+            position: marker.location,
+            city: marker.city
+            
         });
-        newMarker.addEventListener("click", displayMarkerInfo)
+        // when marker clicked then display info
+        newMarker.addListener("click", displayMarkerInfo);
+        return newMarker;
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    /** New MarkerClusterer - initMap function original code https://developers.google.com/maps/documentation/javascript/marker-clustering */
+    // Add a marker clusterer to manage the markers
+    new MarkerClusterer(map, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
 }
-        
-
     
