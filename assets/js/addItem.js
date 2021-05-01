0b35f5001580ae0487266ec9254777597fe0f9f1
gomathishankar28 <gomathishankar28@gmail.com>
2021-05-01 12:57:00 +0000

    created 2 new fileds in place order form and added a new functionality to add items to grocery list textarea in addItem.js

--- assets/js/addItem.js
+++ assets/js/addItem.js
@@ -1,26 +1,18 @@
-function addItem(event) {
+function addItem() {
     event.preventDefault();
-  var li = document.createElement("p");
-  var inputValue = document.getElementById("items").value;
-  /*var t = document.createTextNode(inputValue);
-  li.appendChild(t);*/
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("grocerylist").value = inputValue;
+    console.log("goms");
+  var groceryItem = document.contactForm.grocery_items.value;
+  var quantity = document.contactForm.quantity.value;
+  var list = groceryItem + " " +  "-"  + " " + quantity;
+      list = list.replace(/\r?\n/g, '<br />');
+    if (groceryItem === '') {
+        alert("You must choose atleast one item!");
+    } 
+    if (quantity === '') {
+        alert("You must choose the quantity");
+    } 
+    else {
+        document.contactForm.grocerylist.value +=list;
   }
-  document.getElementById("items").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  return;
 }
