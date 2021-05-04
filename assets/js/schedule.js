0db36aa32b9908ef2de20daa0e5256950ce6d268
gomathishankar28 <gomathishankar28@gmail.com>
2021-05-04 10:58:59 +0000

    added blinking text to schedule

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -44,8 +44,8 @@
     for (let i=0; i<accordionbuttons.length; i++) {
         if (accordionbuttons[i].name === today) {
         accordionbuttons[i].childNodes[1].classList.remove("hide"); // remove hide class from the span element having truck icon to display truck*/
-        accordionbuttons[i].parentElement.nextElementSibling.classList.add("show");    
-        
+        accordionbuttons[i].parentElement.nextElementSibling.classList.add("show");    //to collapse the accordion of the current day.
+        accordionbuttons[i].classList.add("today");
         }
         else {
            console.log("we are closed on weekends"); // No updates made on weekends.
