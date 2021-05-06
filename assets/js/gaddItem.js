c3907f8f2f2bd31f97e5681f7c8519740f7babd6
gomathishankar28 <gomathishankar28@gmail.com>
2021-05-06 06:01:43 +0000

    added a feature to send the grocery list via email

--- assets/js/gaddItem.js
+++ assets/js/gaddItem.js
@@ -4,7 +4,7 @@ function gaddItem() {
    
 var new_field = document.createElement("input"); 
 new_field.setAttribute("type", "text");
-new_field.setAttribute("name", "groceryList");
+
 new_field.setAttribute("class", "inputlist");
     
 var lb = document.createElement("br");
@@ -13,20 +13,18 @@ var lb = document.createElement("br");
 var span1 = document.createElement("span");
   span1.innerText = "X";
   span1.className = "delete";
-  console.log(span1);
+  
 
 var groceryItem = document.contactForm.grocery_items.value;
   var quantity = document.contactForm.quantity.value;
   var groceryList = document.contactForm.grocerylist;
   var list = groceryItem + " " +  "-"  + " " + quantity; 
   new_field.value = list;
-
+ 
 
   x.appendChild(new_field);
   x.appendChild(span1);
-  x.appendChild(lb);
-
-
+  x.appendChild(lb);    
 
 
 var del = document.getElementsByClassName("delete");
@@ -34,11 +32,26 @@ var i;
 for (i = 0; i < del.length; i++) {
   del[i].onclick = function() {
     var div = this.previousSibling;
-    div.style.display = "none";
-    this.style.display = "none";
+    div.remove();
+    this.remove();
+    /*div.style.display = "none";
+    this.style.display = "none";*/
+    
   }
 }
-
-
-}
+var grocList = document.getElementById("grocList");
+    var listInput = document.getElementsByClassName("inputlist");
+    
+    var listDivText = document.getElementsByClassName("inputlist").value;
+    console.log(listInput.length);
+    for (var j=0; j < listInput.length; j++) {
+        
+        var listInputValue = listInput[j].value + '\ \n';
+        console.log(listInputValue);
+        }
+    
+    grocList.textContent += listInputValue;
  
+    
+}
+    
