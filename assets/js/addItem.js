d64f1bfc7226ec4c63a91b501e3d9331feaf92d9
gomathishankar28 <gomathishankar28@gmail.com>
2021-05-08 20:37:48 +0000

    added comments to addItem.js and added styles to schedule

--- assets/js/addItem.js
+++ assets/js/addItem.js
@@ -1,71 +1,60 @@
-function gaddItem() {
-  var x = document.getElementById("groceryList");
-   
-var new_field = document.createElement("input"); 
-new_field.setAttribute("type", "text");
+function addItem() {
 
-new_field.setAttribute("class", "inputlist");
-
-    
-var lb = document.createElement("br");
-
-   
-var span1 = document.createElement("span");
-  span1.innerText = "X";
-  span1.className = "delete";
-  
-
-var groceryItem = document.contactForm.grocery_items.value;
-  var quantity = document.contactForm.quantity.value;
-  var groceryList = document.contactForm.grocerylist;
-  var list = groceryItem + " " +  "-"  + " " + quantity; 
-  new_field.value = list;
+// To get the div element where the added list has to be displayed. 
+    const divList = document.getElementById("groceryList");
  
+// To create an input element for every item being added and set clas and type attribute.
+    var new_field = document.createElement("input"); 
+    new_field.setAttribute("type", "text");
+    new_field.setAttribute("class", "inputlist");
 
-  x.appendChild(new_field);
+// TO create a line break element. 
+    let lb = document.createElement("br");
+
+//  To create a delete button beside every item added.   
+    let span1 = document.createElement("span");
+    span1.innerText = "X";
+    span1.className = "delete";
   
-  x.appendChild(span1);
-  x.appendChild(lb);    
+// To update the input box with item name and quantity.
+    let groceryItem = document.contactForm.grocery_items.value;
+    let quantity = document.contactForm.quantity.value;
+    let groceryList = document.contactForm.grocerylist;
+    let list = groceryItem + " " +  "-"  + " " + quantity; 
+    new_field.value = list;
+ 
+// Appending div elements with input box and delete span element and linebreak
+    divList.appendChild(new_field);
+    divList.appendChild(span1);
+    divList.appendChild(lb);    
 
-  
-
-var del = document.getElementsByClassName("delete");
-var i;
-for (i = 0; i < del.length; i++) {
-  del[i].onclick = function() {
+// To remove an item when clicked on delete span element.
+    var del = document.getElementsByClassName("delete");
+    var i;
+    for (i = 0; i < del.length; i++) {
+    del[i].onclick = function() {
       console.log(grocList.textContent);
-    var item = this.previousSibling;
-    var delItem = item.value;
-    
-     item.remove();
-     this.remove();
-    /*div.style.display = "none";
-    this.style.display = "none";*/
-    
-    var newlist = grocList.textContent.replace(delItem, " ");
-     grocList.textContent = newlist;
+        let item = this.previousSibling;
+        let delItem = item.value;
+        item.remove();
+        this.remove();
+    let newlist = grocList.textContent.replace(delItem, " ");
+    grocList.textContent = newlist;
     console.log(newlist);
   }
   
 }
     
-
-    var grocList = document.getElementById("grocList");
-    var listInput = document.getElementsByClassName("inputlist");
-    
+// To add items to textarea.
+    const grocList = document.getElementById("grocList");
+    let listInput = document.getElementsByClassName("inputlist"); // contains an array of input elements with grocery items.
     console.log(listInput);
-    
     var listDivText = document.getElementsByClassName("inputlist").value;
-    
     console.log(listInput.length);
-    for (var k=0; k < listInput.length; k++) {
-    
-        var listInputValue = listInput[k].value + '\ \n';
+    for (let k=0; k < listInput.length; k++) {
+        let listInputValue = listInput[k].value + '\ \n';
         console.log(listInputValue);
         }
-
-        grocList.textContent += listInputValue;
-
-
-    }
+    grocList.textContent += listInputValue;
+}
     
